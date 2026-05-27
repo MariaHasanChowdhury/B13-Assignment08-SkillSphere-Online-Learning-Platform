69f47dc7ee97588c6d059b69189e032862defd72
MariaHasanChowdhury <mariahasanchowdhury1130@gmail.com>
2026-05-27 12:48:36 +0600

    add courses data in data folder

---
+++ src/app/app/layout.js
@@ -0,0 +1,9 @@
+
+import './globals.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import { Toaster } from 'react-hot-toast'
+
+export const metadata = {
+  title: 'SkillSphere'
+}
